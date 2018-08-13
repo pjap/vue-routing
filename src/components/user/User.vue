6385d4788a541eb1d88c3d77cc7848e5c9a844bc
pjap <pranajaya.pratama@gmail.com>
2018-08-13 15:06:09 +0700

    Create Routing Using Vue Route

---
+++ src/components/user/User.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>This Is User Component</h1>
+    <hr>
+    <button @click="navigateToHome">Go To Home</button>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style >
+</style>
---
+++ src/components/user/UserDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Component User Detail</h1>
+    <p>User Loaded Has ID : {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      :to="'/user/' + $route.params.id + '/edit'"
+      class="btn btn-primary">Edit User
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style >
+</style>
