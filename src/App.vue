99440e081218e7186b0987e547c9e3454c448c56
pjap <pranajaya.pratama@gmail.com>
2018-08-13 15:43:31 +0700

    Mix The Route With An Animation

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
         <h1>Routing</h1>
         <hr>
         <appHeader></appHeader>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in"> 
+          <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -22,4 +24,35 @@ export default {
 </script>
 
 <style>
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  /* transition: opacity 0.5s; */
+}
+.slide-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+.slide-leave-active {
+  transition: opacity 1s ease;
+  opacity: 0;
+  animation: slide-out 1s ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(-30px);
+  }
+}
 </style>
